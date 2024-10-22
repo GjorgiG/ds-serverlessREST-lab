5a9954e3699dd60778e2eb0d930101f4383087b6
Gjorgi <20098721@mail.wit.ie>
2024-10-22 17:54:24 +0100

    Exercise: Delete movie endpoint

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -70,7 +70,7 @@ export class RestAPIStack extends cdk.Stack {
         const deleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", {
           architecture: lambda.Architecture.ARM_64,
           runtime: lambda.Runtime.NODEJS_18_X,
-          entry: `${__dirname}/../lambdas/delete-movie.ts`,
+          entry: `${__dirname}/../lambdas/deleteMovie.ts`,
           timeout: cdk.Duration.seconds(10),
           memorySize: 128,
           environment: {
